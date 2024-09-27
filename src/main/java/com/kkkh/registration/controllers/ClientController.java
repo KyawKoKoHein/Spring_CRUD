13fdd65418b8faf8280610d8724e7eb1867e4fe7
Kyaw Ko Ko Hein <kkkh@mit.com.mm>
2024-09-27 16:56:54 +0630

    edit State and delete State

--- src/main/java/com/kkkh/registration/controllers/ClientController.java
+++ src/main/java/com/kkkh/registration/controllers/ClientController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.kkkh.registration.models.Client;
 import com.kkkh.registration.models.ClientDto;
@@ -22,12 +23,12 @@ import jakarta.validation.Valid;
 @Controller
 @RequestMapping("/clients")
 public class ClientController {
-    
+
     @Autowired
     private ClientRepository clientRepository;
 
-    @GetMapping({"", "/"})
-    public String getClients(Model model){
+    @GetMapping({ "", "/" })
+    public String getClients(Model model) {
         var clients = clientRepository.findAll(Sort.by(Sort.Direction.DESC, "id"));
 
         model.addAttribute("clients", clients);
@@ -35,25 +36,26 @@ public class ClientController {
         return "clients/index";
     }
 
-
-    @GetMapping({"create"})
-    public String createClient(Model model){
+    // Client Dto state
+    @GetMapping({ "create" })
+    public String createClient(Model model) {
         ClientDto clientDto = new ClientDto();
         model.addAttribute("clientDto", clientDto);
-        
+
         return "clients/create";
     }
 
+    // Saving State
     @PostMapping("/create")
-    public String createClient( @Valid @ModelAttribute ClientDto clientDto, BindingResult bResult ){
+    public String createClient(@Valid @ModelAttribute ClientDto clientDto, BindingResult bResult) {
 
-        if(clientRepository.findByEmail(clientDto.getEmail()) != null){
+        if (clientRepository.findByEmail(clientDto.getEmail()) != null) {
             bResult.addError(
-                new FieldError("ClientDto", "email", clientDto.getEmail(), false, null, null, "Email address is already in use!")
-            );
+                    new FieldError("ClientDto", "email", clientDto.getEmail(), false, null, null,
+                            "Email address is already in use!"));
         }
 
-        if(bResult.hasErrors()){
+        if (bResult.hasErrors()) {
             return "clients/create";
         }
 
@@ -67,6 +69,74 @@ public class ClientController {
         client.setCreatedAt(new Date());
 
         clientRepository.save(client);
+
+        return "redirect:/clients";
+    }
+
+    // Edit state
+    @GetMapping("/edit")
+    public String editClient(Model model, @RequestParam int id) {
+        Client client = clientRepository.findById(id).orElse(null);
+
+        if (client == null) {
+            return "redirect:/clients";
+        }
+
+        ClientDto clientDto = new ClientDto();
+        clientDto.setFirstName(client.getFirstName());
+        clientDto.setLastName(client.getLastName());
+        clientDto.setEmail(client.getEmail());
+        clientDto.setPhone(client.getPhone());
+        clientDto.setAddress(client.getAddress());
+        clientDto.setStatus(client.getStatus());
+
+        model.addAttribute("client", client);
+        model.addAttribute("clientDto", clientDto);
+
+        return "clients/edit";
+    }
+
+    //update state
+    @PostMapping("/edit")
+    public String editClient(Model model, @RequestParam int id, @Valid ClientDto clientDto, BindingResult bResult) {
+        Client client = clientRepository.findById(id).orElse(null);
+
+        if (client == null) {
+            return "redirect:/clients";
+        }
+
+        model.addAttribute("client", client);
+
+        if (bResult.hasErrors()) {
+            return "clients/edit";
+        }
+
+        client.setFirstName(clientDto.getFirstName());
+        client.setLastName(clientDto.getLastName());
+        client.setEmail(clientDto.getEmail());
+        client.setPhone(clientDto.getPhone());
+        client.setAddress(clientDto.getAddress());
+        client.setStatus(clientDto.getStatus());
+
+        try {
+            clientRepository.save(client);
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+            bResult.addError(new FieldError("clientDto", "email", clientDto.getEmail(), false, null, null, "Email is already in use!"));
+        }
+
+        return "redirect:/clients";
+    }
+
+    //delete state
+    @GetMapping("/delete")
+    public String deleteClient(@RequestParam int id){
+        Client client = clientRepository.findById(id).orElse(null);
+
+        if(client != null){
+            clientRepository.delete(client);
+        }
 
         return "redirect:/clients";
     }

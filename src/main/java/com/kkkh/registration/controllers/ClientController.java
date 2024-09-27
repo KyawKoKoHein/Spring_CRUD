5aaf376efb40420451bef9dab4f4c7a09f910e09
Kyaw Ko Ko Hein <kkkh@mit.com.mm>
2024-09-27 16:07:21 +0630

    Saving Stat

--- src/main/java/com/kkkh/registration/controllers/ClientController.java
+++ src/main/java/com/kkkh/registration/controllers/ClientController.java
@@ -1,12 +1,23 @@
 package com.kkkh.registration.controllers;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.kkkh.registration.models.Client;
+import com.kkkh.registration.models.ClientDto;
 import com.kkkh.registration.repositories.ClientRepository;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/clients")
@@ -15,10 +26,48 @@ public class ClientController {
     @Autowired
     private ClientRepository clientRepository;
 
-    @GetMapping()
-    public String getClients(){
+    @GetMapping({"", "/"})
+    public String getClients(Model model){
         var clients = clientRepository.findAll(Sort.by(Sort.Direction.DESC, "id"));
 
+        model.addAttribute("clients", clients);
+
         return "clients/index";
+    }
+
+
+    @GetMapping({"create"})
+    public String createClient(Model model){
+        ClientDto clientDto = new ClientDto();
+        model.addAttribute("clientDto", clientDto);
+        
+        return "clients/create";
+    }
+
+    @PostMapping("/create")
+    public String createClient( @Valid @ModelAttribute ClientDto clientDto, BindingResult bResult ){
+
+        if(clientRepository.findByEmail(clientDto.getEmail()) != null){
+            bResult.addError(
+                new FieldError("ClientDto", "email", clientDto.getEmail(), false, null, null, "Email address is already in use!")
+            );
+        }
+
+        if(bResult.hasErrors()){
+            return "clients/create";
+        }
+
+        Client client = new Client();
+        client.setFirstName(clientDto.getFirstName());
+        client.setLastName(clientDto.getLastName());
+        client.setEmail(clientDto.getEmail());
+        client.setPhone(clientDto.getPhone());
+        client.setAddress(clientDto.getAddress());
+        client.setStatus(clientDto.getStatus());
+        client.setCreatedAt(new Date());
+
+        clientRepository.save(client);
+
+        return "redirect:/clients";
     }
 }
--- src/main/java/com/kkkh/registration/models/ClientDto.java
+++ src/main/java/com/kkkh/registration/models/ClientDto.java
@@ -1,83 +1,73 @@
 package com.kkkh.registration.models;
 
-import java.util.Date;
+import jakarta.validation.constraints.*;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-
-@Entity
-@Table(name = "client")
-public class Client {
-    
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-
+public class ClientDto {
+    @NotEmpty(message = "The First Name is required!")
     private String firstName;
+
+    @NotEmpty(message = "The Last Name is required!")
     private String lastName;
 
-    @Column(unique = true, nullable = false)
+    @NotEmpty(message = "Email is required!")
+    @Email
     private String email;
 
     private String phone;
+
     private String address;
-    private String status; //New, Permanent, Lead, Occasional, Inactive
-    private Date createdAt;
-    
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
+
+    @NotEmpty(message="Status is required!")
+    private String status;
+
     public String getFirstName() {
         return firstName;
     }
+
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
+
     public String getLastName() {
         return lastName;
     }
+
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public String getPhone() {
         return phone;
     }
+
     public void setPhone(String phone) {
         this.phone = phone;
     }
+
     public String getAddress() {
         return address;
     }
+
     public void setAddress(String address) {
         this.address = address;
     }
+
     public String getStatus() {
         return status;
     }
+
     public void setStatus(String status) {
         this.status = status;
     }
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
-   
+
 
     
-
 }
